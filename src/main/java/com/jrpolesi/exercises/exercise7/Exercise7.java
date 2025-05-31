36062c47ce6b3a7d086b80df71f06a93f4737d69
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-05-31 17:31:38 -0300

    Add exercise

---
+++ src/main/java/com/jrpolesi/exercises/exercise7/Exercise7.java
@@ -0,0 +1,36 @@
+package com.jrpolesi.exercises.exercise7;
+
+import com.jrpolesi.http.HttpClient;
+
+public class Exercise7 {
+    private static final String URL = "https://apichallenges.eviltester.com/sim/entities/";
+
+    public static void execute() {
+        final var entityID = 10;
+        final var myEntity = Entity.of("atualizado");
+
+        try {
+            final var postResponse = HttpClient.post(URL + entityID, myEntity, Entity.class);
+
+            final var statusCode = postResponse.getStatusCode();
+            System.out.println("Status code: " + statusCode);
+
+            final var entity = postResponse.getBody();
+            System.out.println("Entity atualizada:");
+            System.out.println("-> ID: " + entity.id + ", Name: " + entity.name + ", Description: " + entity.description);
+
+            System.out.println("Buscando entity atualizada...");
+
+            final var getResponse = HttpClient.get(URL + "/" + entity.id, Entity.class);
+
+            final var getStatusCode = getResponse.getStatusCode();
+            System.out.println("Status code: " + getStatusCode);
+
+            final var fetchedEntity = getResponse.getBody();
+            System.out.println("Entity buscada:");
+            System.out.println("-> ID: " + fetchedEntity.id + ", Name: " + fetchedEntity.name + ", Description: " + fetchedEntity.description);
+        } catch (Exception e) {
+            System.err.println("Erro ao executar exercise 7: " + e.getMessage());
+        }
+    }
+}
